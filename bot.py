d2085aa3df4183d4bd239289ead4b6089948d3f0
egor5q <31898567+egor5q@users.noreply.github.com>
2018-05-03 21:26:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,16 @@ token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token)
 
 
+@bot.message_handler(commands=['rules'])
+def rules(m):
+  bot.send_message(m.chat.id, 'Правила проведения турнира:\nВ каждой партии играют 3 команды по 4 человека, у которых для удобства есть свои чат и капитан. Выигрывает та команда, чьи игроки остались в конце партии в живых (или чьих игроков больше), или та команда, чьего суика линчевали. Таким образом, всевозможный руин, как линч пруфа и кросфак - обязательны для данного вида турнира, так как побеждает именно команда, а не один игрок.')
 
 
+@bot.message_handler(commands=['addteam'])
+def dd(m):
+  pass
+  
+  
 if __name__ == '__main__':
   bot.polling(none_stop=True)
 
